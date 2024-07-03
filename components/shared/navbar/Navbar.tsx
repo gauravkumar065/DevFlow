adaa429c2e40f15b8c3aea9dac163370b09895a0
gaurav <gaurav@tokenmetrics.com>
2024-07-03 22:39:27 +0530

    chnged

--- components/shared/navbar/Navbar.tsx
+++ components/shared/navbar/Navbar.tsx
@@ -1,23 +1,18 @@
-import { SignedIn, UserButton } from '@clerk/nextjs'
-import Image from 'next/image'
-import Link from 'next/link'
-import React from 'react'
-import Theme from './Theme'
-import MobileNav from './MobileNav'
-import GlobalSearch from '../search/GlobalSearch'
+import { SignedIn, UserButton } from "@clerk/nextjs";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import Theme from "./Theme";
+import MobileNav from "./MobileNav";
+import GlobalSearch from "../search/GlobalSearch";
 
 const Navbar = () => {
   return (
     <nav className="flex-between background-light900_dark200 fixed z-50 w-full gap-5 p-6 shadow-light-300 dark:shadow-none sm:px-12">
       <Link href="/" className="flex items-center gap-1">
-        <Image
-          src="/assets/images/site-logo.svg"
-          width={23}
-          height={23}
-          alt="DevFlow"
-        />
-
-        <p className="h2-bold font-spaceGrotesk text-dark-100 dark:text-light-900 max-sm:hidden">Dev <span className="text-primary-500">Overflow</span></p>
+        <p className="h2-bold font-spaceGrotesk text-dark-100 dark:text-light-900 max-sm:hidden">
+          Dev <span className="text-primary-400">Flow</span>
+        </p>
       </Link>
 
       <GlobalSearch />
@@ -26,15 +21,15 @@ const Navbar = () => {
         <Theme />
 
         <SignedIn>
-          <UserButton 
+          <UserButton
             afterSignOutUrl="/"
             appearance={{
               elements: {
-                avatarBox: 'h-10 w-10'
+                avatarBox: "h-10 w-10",
               },
               variables: {
-                colorPrimary: '#ff7000'
-              }
+                colorPrimary: "#ff7000",
+              },
             }}
           />
         </SignedIn>
@@ -42,7 +37,7 @@ const Navbar = () => {
         <MobileNav />
       </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
