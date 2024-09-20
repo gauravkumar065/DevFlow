e80be4f191069f6a3980912d76eeb8995f7ec2b4
gaurav <gauravkmr065@gmail.com>
2024-09-20 12:08:54 +0530

    added theme

--- components/shared/NoResult.tsx
+++ components/shared/NoResult.tsx
@@ -1,19 +1,19 @@
-import Image from 'next/image'
-import Link from 'next/link'
-import React from 'react'
-import { Button } from '../ui/button'
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import { Button } from "../ui/button";
 
 interface Props {
-  title: string
-  description: string
-  link: string
-  linkTitle: string
+  title: string;
+  description: string;
+  link: string;
+  linkTitle: string;
 }
 
 const NoResult = ({ title, description, link, linkTitle }: Props) => {
   return (
     <div className="mt-10 flex w-full flex-col items-center justify-center">
-      <Image 
+      <Image
         src="/assets/images/light-illustration.png"
         alt="No result illustration"
         width={270}
@@ -21,7 +21,7 @@ const NoResult = ({ title, description, link, linkTitle }: Props) => {
         className="block object-contain dark:hidden"
       />
 
-      <Image 
+      <Image
         src="/assets/images/dark-illustration.png"
         alt="No result illustration"
         width={270}
@@ -30,15 +30,17 @@ const NoResult = ({ title, description, link, linkTitle }: Props) => {
       />
 
       <h2 className="h2-bold text-dark200_light900 mt-8">{title}</h2>
-      <p className="body-regular text-dark500_light700 my-3.5 max-w-md text-center">{description}</p>
+      <p className="body-regular text-dark500_light700 my-3.5 max-w-md text-center">
+        {description}
+      </p>
 
       <Link href={link}>
-        <Button className="paragraph-medium mt-5 min-h-[46px] rounded-lg bg-primary-500 px-4 py-3 text-light-900 hover:bg-primary-500 dark:bg-primary-500 dark:text-light-900">
+        <Button className="paragraph-medium mt-5 min-h-[46px] rounded-lg bg-black px-4 py-3 text-white dark:bg-white dark:text-black">
           {linkTitle}
         </Button>
       </Link>
     </div>
-  )
-}
+  );
+};
 
-export default NoResult
+export default NoResult;
